cb13769429af7f88bd23098d4ed2acd39531d3cb
Ali Maqsood <alimaqsood35892@gmail.com>
2025-08-08 20:17:29 +0500

    connected to Database

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+// Connect Database
+
+connectDB();
+
+app.co
 app.get('/', (req, res) => {
     res.send("Api running");
 })
 
 app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
