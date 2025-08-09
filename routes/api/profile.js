e0c0cf345adad0d131a2abd1075a483777e1c40c
Ali Maqsood <alimaqsood35892@gmail.com>
2025-08-09 12:21:10 +0500

    fetched github profile through OAuth and added experiecne and education methods

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { response } from "express";
+import dotenv from "dotenv";
 import { check, validationResult } from "express-validator";
+import request from "request";
 import auth from "../../middlewares/auth.js";
 import Profile from "../../models/Profile.js";
 import User from "../../models/User.js";
+
 const router = express.Router();
+
+dotenv.config();
 
 
 //@route GET api/profile/me
@@ -154,5 +159,147 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+//@route PUT api/profile/experience
+//@desc Add profile experience
+//@access Private
+router.put('/experience', [auth, 
+    [
+        check('title', 'Title is required!').not().isEmpty(),
+        check('company', 'Company name is required!').not().isEmpty(),
+        check('from', 'From date is required!').not().isEmpty(),
+    ]
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {title, company, location, from, to, current, description} = req.body;
+
+    const newExp = {
+        title, company, location, from, to, current, description
+    };
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error in PUT Experience!');
+    }
+});
+
+//@route DElETE api/profile/experience/:exp_id
+//@desc Delete profile experience
+//@access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        //Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+//@route PUT api/profile/education
+//@desc Add profile education
+//@access Private
+router.put('/education', [auth, 
+    [
+        check('school', 'School is required!').not().isEmpty(),
+        check('Degree', 'Degree name is required!').not().isEmpty(),
+        check('fieldofstudy', 'Field of Study is required!').not().isEmpty(),
+        check('from', 'From date is required!').not().isEmpty(),
+    ]
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {school, degree, fieldofstudy, from, to, current, description} = req.body;
+
+    const newEdu = {
+        school, degree, fieldofstudy, from, to, current, description
+    };
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+
+        profile.education.unshift(newEdu);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error in PUT Experience!');
+    }
+});
+
+//@route DElETE api/profile/education/:edu_id
+//@desc Delete profile education
+//@access Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        //Get remove index
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+        profile.education.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+//@route GET api/profile/github/:username
+//@desc Get user repos from Github
+//@access Public
+
+
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5
+            &sort=created:asc&client_id=${process.env.GITHUB_CLIENT_ID}
+            &client_secret=${process.env.GITHUB_CLIENT_SECRET}`,
+            method: 'GET',
+            headers: {'user-agent' : 'node.js'}
+        };
+
+        request(options, (error, response, body) => {
+            if(error){
+                console.error(error);
+            }
+
+            if(response.statusCode !== 200){
+                return res.status(404).json({message: 'No Github profile found!'});
+            }
+
+            res.json(JSON.parse(body));
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server in Get Github');
+    }
+})
 
 export default router;
