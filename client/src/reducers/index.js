ada403a40bd5ecc28929642e123d6a7ccfc59a22
Ali Maqsood <alimaqsood35892@gmail.com>
2025-08-19 18:39:19 +0500

    added profile actions, types and reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import alert from "./alert.js"
 import auth from "./auth.js";
-import profile from "./profile.js"
+import profile from "./profile.js";
+import post from "./post.js";
 
 export default combineReducers({
     alert,
     auth,
-    profile
+    profile,
+    post
 });
