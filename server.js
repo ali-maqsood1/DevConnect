0b4ef4b8c8412b3aeb31916f98cc38756ba75e26
Ali Maqsood <alimaqsood35892@gmail.com>
2025-08-19 20:05:29 +0500

    prepared for deployment

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ import users from "./routes/api/users.js";
 import auth from "./routes/api/auth.js";
 import profile from "./routes/api/profile.js";
 import posts from "./routes/api/posts.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
 
 // Connect Database
 connectDB();
@@ -17,16 +19,22 @@ connectDB();
 
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => {
-    res.send("Api running");
-})
 
 //Defining routes
-
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
