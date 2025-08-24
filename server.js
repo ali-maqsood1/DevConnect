e44bba08ea99e0255a92a038ae07ccfedad84bb2
Ali Maqsood <alimaqsood35892@gmail.com>
2025-08-24 21:27:21 +0500

    Deployment ready

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ import auth from "./routes/api/auth.js";
 import profile from "./routes/api/profile.js";
 import posts from "./routes/api/posts.js";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.PORT || 5001;
